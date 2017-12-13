7a44f0c477af38340cedef8bf525407d78730c12
Victor E. Menegon <victor.e.menegon@embraco.com>
2017-12-13 13:53:23 -0200

    -Ideal Six-Step; -BLDC Model working.

--- bldc.m
+++ bldc.m
@@ -5,8 +5,8 @@ clc;
 
 n = 100000;
 
-Tload = -0.1;
-V_barramento = 200;
+Tload = 0.8;
+V_barramento = 100;
 B = 3.2e-4;
 J = 8.7e-4;
 Rs = 4.7;
@@ -16,22 +16,71 @@ Ldq = 20e-3;   % Ld = Lq = L-M %%%%CONFIRMAR ISSO%%%%
 ke = 0.377;
 ke_2 = ke/2;
 kt = 0.377;
-t = 0.0001;    %passo de calculo
+t = 0.00001;    %passo de calculo
 c_360 = 2*pi;
-rst_dt = 1;
-
-
+phase = -pi/6;
 %**************************************************%
 
 for(T = 1:n), 
 
 if (T-1 > 0),
     
-    dt(T) = (dt(T-1) + t);
+    theta_a = theta_e(T-1);
+    theta_b = theta_e(T-1) + (4*c_360)/6;
+    theta_c = theta_e(T-1) + (c_360)/3;
+    
+    ea = F_theta_e(normalize_angle(theta_a));
+    eb = F_theta_e(normalize_angle(theta_b));
+    ec = F_theta_e(normalize_angle(theta_c));
+            
+    Ea(T) = ke * wm(T-1) * ea;
+    Eb(T) = ke * wm(T-1) * eb;
+    Ec(T) = ke * wm(T-1) * ec;
+    
+    if(theta_e(T-1) <= (pi/6+phase)),
+        Va(T) =  0;
+        Vb(T) = -V_barramento/2;
+        Vc(T) = V_barramento/2;
+    elseif(theta_e(T-1) > (pi/6+phase) & theta_e(T-1) <= (pi/2+phase))
+        Va(T) =  V_barramento/2;
+        Vb(T) = -V_barramento/2;
+        Vc(T) = 0;
+    elseif(theta_e(T-1) > (pi/2+phase) & theta_e(T-1) <= (5*pi/6+phase))
+        Va(T) = V_barramento/2;
+        Vb(T) = 0;
+        Vc(T) = -V_barramento/2;
+    elseif(theta_e(T-1) > (5*pi/6+phase) & theta_e(T-1) <= (7*pi/6+phase))
+        Va(T) = 0;
+        Vb(T) = V_barramento/2;
+        Vc(T) = -V_barramento/2;
+    elseif(theta_e(T-1) > (7*pi/6+phase) & theta_e(T-1) <= (3*pi/2+phase))
+        Va(T) = -V_barramento/2;
+        Vb(T) = V_barramento/2;
+        Vc(T) = 0;
+    elseif(theta_e(T-1) > (3*pi/2+phase) & theta_e(T-1) <= (33*pi/18+phase))
+        Va(T) = -V_barramento/2;
+        Vb(T) = 0;
+        Vc(T) = V_barramento/2;     
+    else
+        Va(T) =  0;
+        Vb(T) = -V_barramento/2;
+        Vc(T) = V_barramento/2;
+    end
+    
+    Ta(T) = kt * Ia(T-1) * ea;
+    Tb(T) = kt * Ib(T-1) * eb;
+    Tc(T) = kt * Ic(T-1) * ec;
+    Te(T) = Ta(T) + Tb(T) + Tc(T);
+    
+    dIa(T) = (Va(T) - Rs * Ia(T-1) - Ea(T))*(t/Ldq);
+    dIb(T) = (Vb(T) - Rs * Ib(T-1) - Eb(T))*(t/Ldq);
+    dIc(T) = (Vc(T) - Rs * Ic(T-1) - Ec(T))*(t/Ldq);
     
     dwm(T) = (Te(T) - Tload - B*wm(T-1))*(t/J);
     wm(T) = wm(T-1) + dwm(T);
+    
     dtheta_m(T) = wm(T) * t;
+    
     theta_m(T) = (theta_m(T-1) + dtheta_m(T));
     
     theta_m_norm(T) = normalize_angle(theta_m(T));
@@ -40,44 +89,9 @@ if (T-1 > 0),
     
     theta_e(T) = normalize_angle(theta_e(T));
     
-    theta_a = theta_e(T);
-    theta_b = theta_e(T) + (4*c_360)/6;
-    theta_c = theta_e(T) + (c_360)/3;
-    
-    ea = F_theta_e(normalize_angle(theta_a));
-    eb = F_theta_e(normalize_angle(theta_b));
-    ec = F_theta_e(normalize_angle(theta_c));
-    
-    bla(T) = theta_m(T);
-    
-    Ea(T) = ke_2 * wm(T) * ea;
-    Eb(T) = ke_2 * wm(T) * eb;
-    Ec(T) = ke_2 * wm(T) * ec;
-    
-    Va(T) = V_barramento * ea;
-    Vb(T) = V_barramento * eb;
-    Vc(T) = V_barramento * ec;
-
-    Ta(T) = kt * Ia(T) * ea;
-    Tb(T) = kt * Ib(T) * eb;
-    Tc(T) = kt * Ic(T) * ec;
-    Te(T) = Ta(T) + Tb(T) + Tc(T);
-   
-    
-    dIa(T) = (Va(T) - Rs * Ia(T) - Ea(T))*(t/Ldq);
-    dIb(T) = (Vb(T) - Rs * Ib(T) - Eb(T))*(t/Ldq);
-    dIc(T) = (Vc(T) - Rs * Ic(T) - Ec(T))*(t/Ldq);
-    
     Ia(T) = Ia(T-1) + dIa(T);
     Ib(T) = Ib(T-1) + dIb(T);
     Ic(T) = Ic(T-1) + dIc(T);
-
-    if(theta_m(T) > c_360 | theta_m(T) < 0),
-        rst_dt = 0;
-    else
-        rst_dt = 1;
-    end
-        
 
 time_lapsed(T) = T*t;
     
@@ -88,6 +102,7 @@ else
     dtheta_m = zeros(n,1);
     theta_m = zeros(n,1);
     theta_e = zeros(n,1);
+    theta_m_norm = zeros(n,1);
     dt = zeros(n,1);
    
     Ea = zeros(n,1);
@@ -107,13 +122,12 @@ else
     Tc = zeros(n,1);
     Te = zeros(n,1);
     time_lapsed = zeros(n,1);
-    bla = zeros(n,1);
-    teste_b = zeros(n,1);
+    
 
 end
 end
 
-plot(time_lapsed,bla,'color','g');
+plot(time_lapsed,Te,'color','g');
 
 
 
